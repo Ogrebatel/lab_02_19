adadc78add335e356b6823c6488f3f69d456e254
Ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-07 18:45:37 +0300

    1.0

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,62 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#ifndef LAB_02_19_CASHE_H
+#define LAB_02_19_CASHE_H
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+
+
+#define DIRECT 0
+#define BACKWARD 1
+#define RANDOM 2
+
+#define l1 262144
+#define l3 6291456
+#define iter 100
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+class cache {
+public:
+    cache();
+
+    void start();
+
+    //--------------------------------------EXPERIMENT-----------------------------------------------
+
+    void entry(unsigned number_of_experiment, int type_of_pass);
+
+    void creating(unsigned num_of_experiment);
+
+    void heating(unsigned num_of_experiment);
+
+    //-------------------------------------PASSES-----------------------------------------
+
+    clock_t direct_pass(unsigned num_of_experiment);
+
+    clock_t backward_pass (unsigned num_of_experiment);
+
+    clock_t random_pass (unsigned num_of_experiment);
+
+    //----------------------------------ALL_PASSES_HIGHER------------------------------------------
+
+    void clearing();
+
+    //---------------------------------------------------------------------------------------------
+
+    void results(string type_of_pass, unsigned number_of_experiment, clock_t time);
+
+    vector<long> cache_size_of_experiment;
+    char* buffer;
+};
+
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,140 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+
+cache::cache() {
+    int x = 1;
+
+    for (; pow(2, x) < 0.5 * l1; ++x);
+
+    int n = 0;
+    while (pow(2, x + n) < (1.5) * l3) {
+        cache_size_of_experiment.push_back(pow(2, x + n));
+        ++n;
+    }
+
+    cache_size_of_experiment.push_back(1.5 * l3);
+
+    for (int i = 0; i < cache_size_of_experiment.size(); ++i)
+        cout << i << " experiment: size = " << cache_size_of_experiment[i] << endl;
+}
+
+void cache::start() {
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 3; ++j)
+            entry(i, j);
+    }
+}
+
+//--------------------------------------EXPERIMENT-----------------------------------------------
+
+void cache::entry(unsigned number_of_experiment, int type_of_pass) {
+
+    clock_t time;
+    creating(number_of_experiment);
+    heating(number_of_experiment);
+    switch (type_of_pass) {
+        case DIRECT:
+            time = direct_pass(number_of_experiment);
+            results("direct", number_of_experiment, time);
+            break;
+        case BACKWARD:
+            time = backward_pass(number_of_experiment);
+            results("backward", number_of_experiment, time);
+            break;
+        case RANDOM:
+            time = random_pass(number_of_experiment);
+            results("random", number_of_experiment, time);
+            break;
+    }
+
+    clearing();
+}
+
+void cache::creating(unsigned num_of_experiment) {
+    buffer = new char[cache_size_of_experiment[num_of_experiment]];
+}
+
+void cache::heating(unsigned num_of_experiment) {
+    for (unsigned i = 0; i < iter; ++i) {
+        for (unsigned num = 0; num < cache_size_of_experiment[num_of_experiment]; ++num) {
+            buffer[num] = (char) (rand_r(0) % 255 - 128);
+        }
+    }
+}
+
+//-------------------------------------PASSES-----------------------------------------
+
+clock_t cache::direct_pass(unsigned num_of_experiment) {
+    clock_t start = clock();
+    for (unsigned i = 0; i < iter; ++i) {
+        for (unsigned num = 0; num < cache_size_of_experiment[num_of_experiment]; ++num) {
+            buffer[num] = (char) (rand_r(0) % 255 - 128);
+        }
+    }
+    clock_t stop = clock();
+    return stop - start;
+}
+
+clock_t cache::backward_pass(unsigned num_of_experiment) {
+
+    clock_t start = clock();
+    for (unsigned i = 0; i < iter; ++i) {
+        for (unsigned num = cache_size_of_experiment[num_of_experiment]; num > 0; --num) {
+            buffer[num] = (char) (rand_r(0) % 255 - 128);
+        }
+    }
+    clock_t stop = clock();
+    return stop - start;
+}
+
+clock_t cache::random_pass(unsigned num_of_experiment) {
+    vector<int> current_num;
+
+    for (unsigned i = 1; i < cache_size_of_experiment[num_of_experiment]; ++i) {
+        current_num.push_back(i);
+    }
+
+    srand(time(NULL));
+    random_shuffle(current_num.begin(), current_num.end());
+
+    clock_t start = clock();
+    for (unsigned i = 0; i < iter; ++i) {
+        for (unsigned num = 0; num <= cache_size_of_experiment[num_of_experiment]; ++num) {
+            buffer[current_num[num]] = (char) (rand_r(0) % 255 - 128);
+
+        }
+    }
+
+    clock_t stop = clock();
+    return stop - start;
+}
+
+//----------------------------------ALL_PASSES_HIGHER------------------------------------------
+
+void cache::clearing() {
+    cache_size_of_experiment.clear();
+    delete buffer;
+}
+
+//---------------------------------------------------------------------------------------------
+
+void cache::results(string type_of_pass, unsigned number_of_experiment, clock_t time) {
+    cout << "investigation:" << endl;
+    cout << "  travel_variant: " << type_of_pass << endl;
+    cout << "  experiments: " << endl;
+    cout << "     - experiment: " << endl;
+    cout << "  number: " << number_of_experiment << endl;
+    cout << "  input_data: " << endl;
+    cout << "    buffer_size: " << cache_size_of_experiment[number_of_experiment] << " bytes" << endl;
+    cout << "  results: " << std::endl;
+    cout << "duration: " << time << " ms" << endl << endl;
+}
+
+int main() {
+
+    cache experiment;
+    experiment.start();
+
+    return 0;
+}
